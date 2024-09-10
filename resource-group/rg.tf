a9512fc7f74e5bbef51b002635f39b8a86933301
Sumith Mohan <smohan@berkadia.com>
2024-09-10 18:33:32 +0530

    initial commit

---
+++ resource-group/rg.tf
@@ -0,0 +1,26 @@
+// main.tf
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+// variables.tf
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+  default     = "East US"
+}
+
+// outputs.tf
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
